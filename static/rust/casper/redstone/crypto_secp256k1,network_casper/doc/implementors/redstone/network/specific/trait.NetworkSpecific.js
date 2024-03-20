00d69a897836a0932ed57ad93d60ec21152df53f
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-03-20 13:30:28 +0100

    feat: added casper network

---
+++ static/rust/casper/redstone/crypto_secp256k1,network_casper/doc/implementors/redstone/network/specific/trait.NetworkSpecific.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"redstone":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ static/rust/casper/redstone/crypto_secp256k1,network_casper/doc/redstone/network/casper/contracts/contract/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["contract_install"],"trait":["Contract"]};
