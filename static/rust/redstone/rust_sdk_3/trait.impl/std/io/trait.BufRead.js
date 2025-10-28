bbfe613716ea5acf4e8a72b793f12882cb774b8c
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-10-28 11:38:56 +0100

    Update Rust SDK documentation (#147)

---
+++ static/rust/redstone/rust_sdk_3/trait.impl/std/io/trait.BufRead.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stellar_xdr",[["impl&lt;R: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a> for <a class=\"struct\" href=\"stellar_xdr/curr/struct.Limited.html\" title=\"struct stellar_xdr::curr::Limited\">Limited</a>&lt;R&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[435]}
