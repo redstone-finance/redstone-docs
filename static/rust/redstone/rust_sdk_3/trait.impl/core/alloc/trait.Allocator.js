830968d33b3d145df0281938912633bd094f0289
Jakub Leszczak <jakub.leszczak@redstone.finance>
2025-10-28 09:51:46 +0100

    Update Rust SDK documentation

---
+++ static/rust/redstone/rust_sdk_3/trait.impl/core/alloc/trait.Allocator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["radix_common",[]],["radix_rust",[]],["sbor",[]],["scrypto",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[19,18,12,15]}
---
+++ static/rust/redstone/rust_sdk_3/trait.impl/core/fmt/trait.Write.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ark_std",[]],["arrayvec",[["impl&lt;const CAP: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"]]],["either",[["impl&lt;L, R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"enum\" href=\"either/enum.Either.html\" title=\"enum either::Either\">Either</a>&lt;L, R&gt;<div class=\"where\">where\n    L: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a>,\n    R: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a>,</div>"]]],["radix_common",[]],["radix_rust",[]],["sbor",[]],["scrypto",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[14,405,582,20,18,12,15]}
